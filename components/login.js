f481ffb6212691465220e94e2f97cd2491a3b6d0
Insheloots <divher566@gmail.com>
2020-09-21 19:34:00 -0500

    vista 1

--- components/login.js
+++ components/login.js
@@ -3,49 +3,59 @@ import { Text, View, StyleSheet, Image, TextInput, Button} from 'react-native';
 
 export default function login({navigation}){
     return(
-        <View>
-        <Image style={styles.img} source = {require('../src/imgs/logo.png')}/>
-        <TextInput placeholder=" Ingresa tu correo electrónico" style={styles.textbox}/>
-        <TextInput placeholder=" Contraseña" secureTextEntry={true} style={styles.textbox}/>
-        <View style={styles.btn}>
-        <Button title="Ingresar" color='#3491cd' onPress={() => navigation.navigate('Consejo')}>Registrate</Button>
+        <View style={styles.container}>
+          <View style={styles.profileimage}>
+            <Image style={styles.img} source = {require('../src/imgs/logo.png')}/>
+          </View>
+        <View style={styles.center}>
+          <TextInput placeholder=" Ingresa tu correo electrónico" style={styles.textbox}/>
+          <TextInput placeholder=" Contraseña" secureTextEntry={true} style={styles.textbox}/>
         </View>
-        <Text style={styles.blacktext}>¿No tienes una cuenta?</Text>
         <View style={styles.btn}>
-        <Button title="Registrate" color="#3491cd" onPress={() => navigation.navigate('Registro')}>Registrate</Button>
+          <Button title="Ingresar" color='#3491cd' onPress={() => navigation.navigate('Consejo')}>Registrate</Button>
+          <Text style={styles.blacktext}>¿No tienes una cuenta?</Text>
+          <Button title="Registrate" color="#3491cd" onPress={() => navigation.navigate('Registro')}>Registrate</Button>
         </View>
       </View>
     )
 }
       
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  }, 
+  profileimage: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    width: '80%',
+  },
   blacktext:{
     fontSize: 14,
-    textAlign: 'center',
     fontStyle: 'normal',
-    marginTop: '10%',
-    marginBottom: '-5%',
     color: 'black',
+    margin: '2%',
   },
   textbox:{
-    textAlign: 'justify',
-    marginHorizontal: '14%',
-    marginVertical: '4%',
+    margin: "2%",
     borderColor: 'black',
     fontSize: 14,
     borderWidth: 0.3,
+    borderRadius: 10,
     height: 50,
   },
   btn:{
-    textAlign: 'center',
-    marginTop: '10%',
-    marginHorizontal: '9%', 
-    width: 294,
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    width: '80%',
   },
-  img:{
-    marginTop: '5%',
-    marginBottom: '10%',
-    marginHorizontal: '14%', 
+  img:{ 
     resizeMode: 'contain',
     width: 260
   }
