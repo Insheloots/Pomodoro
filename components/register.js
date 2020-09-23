310f44ba97cd30ed4b4e894f56088dc7fa2c8f4b
Insheloots <divher566@gmail.com>
2020-09-22 23:41:55 -0500

    Tercer Sprint

--- components/register.js
+++ components/register.js
@@ -3,18 +3,26 @@ import { Text, View, StyleSheet, TextInput, Button, Alert } from 'react-native';
 
 export default function register({navigation}){
     return(
-        <View>
-        <Text style={{fontSize: 18, textAlign: 'center', fontStyle: 'normal', fontWeight: 'bold', marginVertical:'7%', marginHorizontal:'10%', color: 'black'}}>
-        Para disfrutar de nuestro aplicativo, debes primero registrarte</Text>
-        <TextInput placeholder=" Ingresa tu nombre(s)" style={styles.textbox}/>
-        <TextInput placeholder=" Ingresa tu apellido(s)" style={styles.textbox}/>
-        <TextInput placeholder=" Ingresa tu correo electrónico" style={styles.textbox}/>
-        <TextInput placeholder=" Ingresa tu contraseña" secureTextEntry={true} style={styles.textbox}/>
-        <View style={styles.btn}>
-        <Button style={styles.btn} title="Registrar" color='#3491cd' onPress={() => Alert.alert('Te has registrado correctamente')}/>
+        <View style={styles.container}>
+          <View style={styles.top}>
+            <Text style={{fontSize: 18, textAlign: 'center', fontStyle: 'normal', fontWeight: 'bold', marginVertical:'7%', marginHorizontal:'10%', color: 'black'}}>
+              Para disfrutar de nuestro aplicativo, debes primero registrarte</Text>
+          </View>
+          <View style={styles.center}>
+            <Text> Nombre</Text>
+            <TextInput placeholder="  Ingresa tu nombre(s)" style={styles.textbox}/>
+            <Text> Apellidos</Text>
+            <TextInput placeholder="  Ingresa tu apellido(s)" style={styles.textbox}/>
+            <Text> Correo electronico</Text>
+            <TextInput placeholder="  Ingresa tu correo electrónico" style={styles.textbox}/>
+            <Text> Contraseña</Text>
+            <TextInput placeholder="  Ingresa tu contraseña" secureTextEntry={true} style={styles.textbox}/>
+          </View>
+        <View style={styles.bottom}>
+          <Button title="Registrar" onPress={() => Alert.alert('Te has registrado correctamente')}/>
         </View>
         <View style={styles.btn}>
-        <Button title="Volver" color='#3491cd' onPress={() => navigation.navigate('Inicio de Sesion')}>Volver</Button>
+          <Button style={styles.btn} title="Volver" onPress={() => navigation.navigate('Inicio de Sesion')}>Volver</Button>
         </View>
       </View>
     )
@@ -22,6 +30,25 @@ export default function register({navigation}){
 
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  top: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  center: {
+    flexDirection: "column",
+    width: "80%",
+  },
+  bottom: {
+    flex: 1,
+    margin: "auto",
+    flexDirection: "column",
+    justifyContent: "center",
+    width: "75%",
+  },  
   blacktext:{
     fontSize: 14,
     marginHorizontal:33,
@@ -39,18 +66,15 @@ const styles = StyleSheet.create({
     color: 'black',
   },
   textbox:{
-    textAlign: 'justify',
-    marginHorizontal: '14%',
     marginVertical: '3%',
     borderColor: 'black',
     fontSize: 14,
-    borderWidth: 0.2,
+    borderRadius: 13,
+    borderWidth: 0.4,
     height: 50,
   },
-  btn:{
-    textAlign: 'center',
-    marginTop: '5%',
-    marginHorizontal: '9%', 
-    width: 294,
+  btn: {
+    flex: 1,
+    width: "75%",
   },
 });
